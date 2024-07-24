d782ac5c02f29d3cb6001e200ba0b3efc3a482ea
tin6150 <sn+newbox@grumpyxmas.com>
2024-07-24 14:48:32 -0700

    based on py-conda

--- README.md
+++
@@ -1,2 +0,0 @@
-# prodigal
-unofficial containerization of the protein gene prediction program prodigal
---
+++ install_app.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+conda install bioconda::prodigal
+# conda install bioconda/label/cf201901::prodigal
